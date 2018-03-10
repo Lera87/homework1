e7e4c3373c328363ce46ce9cbb4d00396a232a09
lera <x@y.z>
2018-03-10 21:35:10 +0200

    dummy commit

--- homework7-8.py
+++ homework7-8.py
@@ -9,3 +9,36 @@ name, surname = person_name.split(" ")
 new_person_name = "{} {}".format(surname, name)
 print(person_name)
 print(new_person_name)
+
+#задание7
+american_date = "05.17.2016"
+month = int(american_date[:2])
+day = int(american_date[3:5])
+year = int(american_date[6:])
+european_date = "{}.{}.{}".format(day, month, year)
+print(american_date)
+print(european_date)
+
+#задание9
+snake_case = "employee_first_name"
+new = snake_case.split("_")
+camelcase = new[0].capitalize()+new[1].capitalize()+new[2].capitalize()
+print(snake_case)
+print(camelcase)
+
+#задание10
+writer = "Leo Tolstoy*1828-08-28*1910-11-20"
+#writer = "Marcus Aurelius*121-04-26*180-03-17"
+name, DOB, DOD = writer.split("*")
+DOB1 = DOB.split("-")
+DOD1 = DOD.split("-")
+age = int(DOD1[0])-int(DOB1[0])
+print(name,",", age)
+
+
+
+
+
+
+
+

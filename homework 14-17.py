92c8df5f9c40ae5da64dc301dc445481192be017
Lera87 <36897771+Lera87@users.noreply.github.com>
2018-03-14 14:17:00 +0200

    Add files via upload
    14 задание

---
+++ homework 14-17.py
@@ -0,0 +1,18 @@
+#задание 14
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+print(is_even(7))
+
+#if is_even(8):
+#     print("The number is even!")
+# else:
+#     print("The number is odd!")
+
+
+
+
+

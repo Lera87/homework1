dd7e03f78d7dcd7724a59ce6b8107d27e9a112d4
Lera87 <36897771+Lera87@users.noreply.github.com>
2018-03-30 18:12:45 +0300

    added task 21

--- homework 14-21.py
+++ homework 14-21.py
@@ -1,7 +1,11 @@
 import math
 import random
 
-#задание 14
+# задание 14
+# Написать функцию, которая будет проверять четность некоторого числа.
+# def is_even(number): # returns boolean value
+#     pass
+
 def is_even(number):
     if number % 2 == 0:
         return True
@@ -19,7 +23,11 @@ print(is_even(8))
 print("----------------------------------------")
 
 
-#задание 15
+# задание 15
+# Написать функцию, которая отвечает на вопрос, пересекаются ли две заданные окружности на плоскости. Каждая окружность задается координатами центра и радиусом.
+# def circles_intersect(x1, y1, r1, x2, y2, r2): # returns boolean value
+#     pass
+
 def circles_intersects(x1, y1, r1, x2, y2, r2):
     distance_between_centers = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
     if distance_between_centers == r1 + r2 or distance_between_centers == abs(r1 - r2):
@@ -35,7 +43,11 @@ print(circles_intersects(3, 4, 10, 14, 18, 5))
 print("----------------------------------------")
 
 
-#задание 16
+# задание 16
+# Два поезда движутся на скорости V1 и V2 навстречу друг другу. Между ними 10 км. пути. Через 4 км пути первый поезд может свернуть на запасной путь. При заданных скоростях узнать столкнутся ли поезда.
+# def have_trains_crashed(v1, v2): # returns boolean value
+#     pass
+
 def have_trains_crashed(v1, v2):
     total_distance = 10
     first_segment = 4
@@ -57,7 +69,11 @@ else:
 
 print("----------------------------------------")
 
-#задание 17
+# задание 17
+# Написать функцию решения квадратного уравнения.
+# def solve_quadratic_equation(a, b, c): # returns 2 values: either 2 roots, 1 root and None or 2 Nones
+#     pass
+
 print("Решение квадратного уравнения (ax ** 2 + bx + c = 0): ")
 
 def solve_quadratic_equation(a, b, c):
@@ -85,7 +101,11 @@ else:
 print("----------------------------------------")
 
 
-#задание 18
+# задание 18
+# Каждому символу в таблице символов Unicode соответствует число. Написать функцию, которая рассчитывает сумму чисел, которые соответствуют символам, стоящим между двумя заданными включительно. Например, в функцию передаются символы ‘x’ и ‘z’. Значит надо вычислить сумму кодов символов ‘x’,’y’,’z’.
+# def sum_symbol_codes(first, last): # returns int
+#     pass
+
 def sum_symbol_codes(first, last):
     unicode1 = ord(first)
     unicode2 = ord(last)
@@ -101,7 +121,11 @@ print(result)
 
 print("----------------------------------------")
 
-#задание 19
+# задание 19
+# Написать функцию для поиска разницы между максимальным и минимальным числом среди num_limit случайно сгенерированных чисел в указанном числовом диапазоне.
+# def diff_min_max(num_limit, lower_bound, upper_bound): # returns int
+#     pass
+
 def diff_min_max(num_limit, lower_bound, upper_bound):
     current_min = upper_bound
     current_max = lower_bound
@@ -121,7 +145,12 @@ print("Difference of max and min:", diff_min_max(5, 100, 200))
 
 print("----------------------------------------")
 
-#задание 20
+# задание 20
+# Написать функцию для поиска разницы сумм всех четных и всех нечетных чисел среди num_limit случайно сгенерированных чисел в указанном числовом диапазоне. Т.е. от суммы четных чисел вычесть сумму нечетных чисел.
+# def diff_even_odd(num_limit, lower_bound, upper_bound): # returns int
+#     pass
+
+
 def diff_even_odd(num_limit, lower_bound, upper_bound):
     total_even = 0
     total_odd = 0
@@ -137,9 +166,37 @@ def diff_even_odd(num_limit, lower_bound, upper_bound):
     return total_even - total_odd
 
 print("###################")
-print("Difference of even and odd:", diff_even_odd(10, 50, 100))
+print("Difference between even and odd:", diff_even_odd(10, 50, 100))
 
 print("----------------------------------------")
+
+# задание 21
+# Написать функцию возвращающую наибольшую цифру случайно сгенерированного 12 ти-значного натурального числа.
+# def get_max_digit(number): # returns int
+#     pass
+
+rand_num = random.randint(100000000000, 999999999999)
+print("Случайно сгенерированное 12-значное число:", rand_num)
+
+def get_max_digit(rand_num):
+    rand_num_str = str(rand_num)
+    max_number = "0"
+    for digit in rand_num_str:
+        if digit > max_number:
+            max_number = digit
+
+    return max_number
+
+print("Max number is :",get_max_digit(rand_num))
+
+print("----------------------------------------")
+
+
+
+
+
+
+
 
 
 
